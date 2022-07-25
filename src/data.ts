7995e2a90a9c36bb1face6866d767df3b238ec1e
R0ixy <nikita6kom@gmail.com>
2022-07-25 19:06:01 +0300

    updated texts

--- src/data.ts
+++ src/data.ts
@@ -1,11 +1,11 @@
 export const texts = [
-  "Text for typing #1",
-  "Text for typing #2",
-  "Text for typing #3",
-  "Text for typing #4",
-  "Text for typing #5",
-  "Text for typing #6",
-  "Text for typing #7",
+    "Text for typing #1 Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    "Text for typing #2 consectetur adipiscing elit, sed do eiusmod",
+    "Text for typing #3 tempor incididunt labore et dolore magna aliqua",
+    "Text for typing #4 Ut enim ad minim veniam, quis nostrud exercitation",
+    "Text for typing #5 ullamco laboris nisi ut aliquip ex ea commodo consequat",
+    "Text for typing #6 Duis aute irure dolor in reprehenderit in voluptate velit",
+    "Text for typing #7 esse cillum dolore eu fugiat nulla pariatur",
 ];
 
 export default { texts };
